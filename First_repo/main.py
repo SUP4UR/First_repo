25ac9c98964477804096ca06ff28b3a4af386ef8
SUP4UR <harcenkovlad2703@gmail.com>
2025-04-23 17:54:16 +0200

    update main.py

--- First_repo/main.py
+++ First_repo/main.py
@@ -1,2 +1,36 @@
-print("Hello world")
-print("Hi world")
+from datetime import datetime
+
+def get_days_from_today(date):
+    """
+    Розраховує кількість днів між заданою датою і поточною датою.
+
+    :param date: str, дата у форматі 'РРРР-ММ-ДД'.
+    :return: int, кількість днів від заданої дати до поточної.
+    """
+    try:
+        # Перетворення рядка дати на об'єкт datetime
+        given_date = datetime.strptime(date, '%Y-%m-%d').date()
+        # Отримання поточної дати
+        today = datetime.today().date()
+        # Обчислення різниці в днях
+        delta = today - given_date
+        return delta.days
+    except ValueError:
+        raise ValueError("Дата повинна бути у форматі 'РРРР-ММ-ДД'")
+
+# Приклад використання функції
+if __name__ == "__main__":
+    try:
+        example_date = "2021-10-09"
+        print(get_days_from_today(example_date))
+    except ValueError as e:
+        print(e)
+
+
+
+
+
+
+
+
+
