9d74479b316132727c9a6bc35b8c2ff3efe21789
SUP4UR <harcenkovlad2703@gmail.com>
2025-05-03 23:53:55 +0200

    Update main.py

--- First_repo/main.py
+++ First_repo/main.py
@@ -1,30 +1,22 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    """
-    Розраховує кількість днів між заданою датою і поточною датою.
-
-    :param date: str, дата у форматі 'РРРР-ММ-ДД'.
-    :return: int, кількість днів від заданої дати до поточної.
-    """
     try:
-        # Перетворення рядка дати на об'єкт datetime
         given_date = datetime.strptime(date, '%Y-%m-%d').date()
-        # Отримання поточної дати
         today = datetime.today().date()
-        # Обчислення різниці в днях
         delta = today - given_date
         return delta.days
     except ValueError:
-        raise ValueError("Дата повинна бути у форматі 'РРРР-ММ-ДД'")
+        print("Помилка: Дата повинна бути у форматі 'РРРР-ММ-ДД'")
+        return None
 
-# Приклад використання функції
 if __name__ == "__main__":
-    try:
-        example_date = "2021-10-09"
-        print(get_days_from_today(example_date))
-    except ValueError as e:
-        print(e)
+    example_date = "13.14.2000"
+    result = get_days_from_today(example_date)
+    if result is not None:
+        print(result)
+
+
 
 
 
