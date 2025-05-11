975dcf92d99264250e61e2f7624d4913d75f9ce9
SUP4UR <harcenkovlad2703@gmail.com>
2025-05-11 20:11:46 +0200

    update main.py

--- First_repo/main.py
+++ First_repo/main.py
@@ -1,20 +1,21 @@
-from datetime import datetime
+import random
 
-def get_days_from_today(date):
-    try:
-        given_date = datetime.strptime(date, '%Y-%m-%d').date()
-        today = datetime.today().date()
-        delta = today - given_date
-        return delta.days
-    except ValueError:
-        print("Помилка: Дата повинна бути у форматі 'РРРР-ММ-ДД'")
-        return None
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000:
+        print("Помилка: межі діапазону повинні бути від 1 до 1000.")
+        return []
+    if min > max:
+        print("Помилка: мінімальне значення не може бути більшим за максимальне.")
+        return []
+    if quantity < 1:
+        print("Помилка: кількість чисел має бути не менше 1.")
+        return []
+    if quantity > (max - min + 1):
+        print("Помилка: кількість чисел перевищує кількість доступних унікальних значень у діапазоні.")
+        return []
+    numbers = random.sample(range(min, max + 1), quantity)
+    return sorted(numbers)
 
-if __name__ == "__main__":
-    example_date = "13.14.2000"
-    result = get_days_from_today(example_date)
-    if result is not None:
-        print(result)
 
 
 
