b46b07653b80af1806428cfed12fa969e6b3bfbc
SUP4UR <harcenkovlad2703@gmail.com>
2025-06-04 22:14:55 +0200

    update main.py

--- First_repo/main.py
+++ First_repo/main.py
@@ -1,29 +1,50 @@
-import random
+def total_salary(path):
+    try:
+        total = 0
+        count = 0
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                try:
+                    name, salary = line.strip().split(',')
+                    total += int(salary)
+                    count += 1
+                except ValueError:
+                    print(f"Неправильний формат рядка: {line.strip()}")
+                    continue
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000:
-        print("Помилка: межі діапазону повинні бути від 1 до 1000.")
+        if count == 0:
+            return 0, 0  
+        average = total // count
+        return total, average
+
+    except FileNotFoundError:
+        print(f"Файл за шляхом '{path}' не знайдено.")
+        return 0, 0
+    except Exception as e:
+        print(f"Сталася помилка при обробці файлу: {e}")
+        return 0, 0
+    
+
+
+    def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if len(parts) == 3:
+                    cat_id, name, age = parts
+                    cat_dict = {"id": cat_id, "name": name, "age": age}
+                    cats_list.append(cat_dict)
+                else:
+                    print(f"Пропущено рядок з некоректним форматом: {line.strip()}")
+        return cats_list
+    except FileNotFoundError:
+        print(f"Файл за шляхом '{path}' не знайдено.")
         return []
-    if min > max:
-        print("Помилка: мінімальне значення не може бути більшим за максимальне.")
+    except Exception as e:
+        print(f"Виникла помилка при читанні файлу: {e}")
         return []
-    if quantity < 1:
-        print("Помилка: кількість чисел має бути не менше 1.")
-        return []
-    if quantity > (max - min + 1):
-        print("Помилка: кількість чисел перевищує кількість доступних унікальних значень у діапазоні.")
-        return []
-    numbers = random.sample(range(min, max + 1), quantity)
-    return sorted(numbers)
 
 
-
-
-
-
-
-
-
-
-
-
+                
